b69f0d444522de11d683951cce8f965cbd14f1c7
Sami Tapalinen <t1tasa01@students.oamk.fi>
2023-02-22 22:51:08 +0200

    Add reach client and node.js express server

---
+++ server/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const port = 5001;
+const cors = require("cors");
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if ((!origin, callback)) {
+      callback(null, true);
+    } else {
+      callback(new Error("not allowed bycors"));
+    }
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.get("/api/", (req, res) => {
+  res.send("Hello World!");
+});
+app.post("/api/get-data", (req, res) => {
+  res.send({ text: "webapp.io" });
+});
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
